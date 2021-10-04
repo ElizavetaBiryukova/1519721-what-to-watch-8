d39bca1b5f34705cdba33662df54322c0b7c9dc7
ElizavetaBiryukova <birliza@gmail.com>
2021-10-04 17:35:41 +0200

    Добавляет React-компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const InfoPromoFilm = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE_DATE: 2014,
+  CARDS_COUNT: 20,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={InfoPromoFilm.TITLE}
+      genre={InfoPromoFilm.GENRE}
+      releaseDate={InfoPromoFilm.RELEASE_DATE}
+      cardsCount={InfoPromoFilm.CARDS_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
