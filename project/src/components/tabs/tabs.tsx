f38eb1fff8df1dd595a59e65d2221e742b92d6b0
ElizavetaBiryukova <birliza@gmail.com>
2021-10-24 23:24:25 +0200

    Мапит список табов

--- project/src/components/tabs/tabs.tsx
+++ project/src/components/tabs/tabs.tsx
@@ -1,32 +1,34 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import TabsOverviews from './tabs-overviews';
 import TabsDetails from './tabs-details';
 import TabsReviews from './tabs-reviews';
-import { Film } from '../../types/films';
-import { Reviews } from '../../types/reviews';
+import {Film} from '../../types/films';
+import {Reviews} from '../../types/reviews';
 
 type TabsProps = {
   film: Film;
   reviews: Reviews;
 }
 
+const tabsTitles = [
+  {title: 'Overview'},
+  {title: 'Details'},
+  {title: 'Reviews'},
+];
+
 function Tabs(props: TabsProps): JSX.Element {
-  const { film, reviews } = props;
+  const {film, reviews} = props;
 
-  const [toggleState, setToggleState] = useState(1);
+  const [toggleState, setToggleState] = useState(0);
 
-  const toggleTab = (index: number) => {
-    setToggleState(index);
-  };
-
-  const getToggleTab = (tab: number) => {
-    switch (tab) {
+  const getToggleTab = (index: number) => {
+    switch (index) {
+      case 0:
+        return <TabsOverviews film={film} />;
       case 1:
-        return <TabsOverviews film={film}/>;
+        return <TabsDetails film={film} />;
       case 2:
-        return <TabsDetails film={film}/>;
-      case 3:
-        return <TabsReviews reviews={reviews}/>;
+        return <TabsReviews reviews={reviews} />;
     }
   };
 
@@ -34,24 +36,14 @@ function Tabs(props: TabsProps): JSX.Element {
     <div className="film-card__desc">
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
-          <li
-            className={toggleState === 1 ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}
-            onClick={() => toggleTab(1)}
-          >
-            <span className="film-nav__link">Overview</span>
-          </li>
-          <li
-            className={toggleState === 2 ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}
-            onClick={() => toggleTab(2)}
-          >
-            <span className="film-nav__link">Details</span>
-          </li>
-          <li
-            className={toggleState === 3 ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}
-            onClick={() => toggleTab(3)}
-          >
-            <span className="film-nav__link">Reviews</span>
-          </li>
+          {tabsTitles.map((item, index) => (
+            <li key={item.title}
+              className={toggleState === index ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}
+              onClick={() => setToggleState(index)}
+            >
+              <span className="film-nav__link">{item.title}</span>
+            </li>
+          ))}
         </ul>
       </nav>
       {getToggleTab(toggleState)}
